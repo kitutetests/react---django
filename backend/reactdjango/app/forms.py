36ced88fb817654924a246d0cf46e97eb57061c6
jeffkitute <geffmutua001@gmail.com>
2024-03-05 18:41:50 +0300

    css for small screen

--- backend/reactdjango/app/forms.py
+++ backend/reactdjango/app/forms.py
@@ -1,19 +1,22 @@
 from django import forms
+
 from .models import Property_for_renting,Property_on_sale
 
 class HouseRentForm(forms.ModelForm):
    
     class Meta:
         model = Property_for_renting
-        fields = ['size','apartment_name','location','features','photos','video','price','deposit','water_fee','garbage_fee','landlord_contact','caretaker_contact','agent_name','agent_contact']
-
-
+        fields = ['size','apartment_name','location','pin_location','features','photos','video','price','deposit','water_fee','garbage_fee','landlord_contact','caretaker_contact','agent_name','agent_contact']
+        widgets = {'pin_location': forms.TextInput(attrs={'placeholder':  '(optional)'}),
+                 
+        }
     
 class SellPropertyForm(forms.ModelForm):
    
     class Meta:
         model = Property_on_sale
-        fields = ['property_type','features','location','photos','video','price','payment_process','contact']
-
+        fields = ['property_type','features','location','pin_location','photos','video','price','payment_process','contact']
+        widgets = {'pin_location': forms.TextInput(attrs={'placeholder': '(optional)'}),
+        }
 
   
