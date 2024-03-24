ab499add4c150c5ace1d13ba13bf7fdc41ebec98
jeffkitute <geffmutua001@gmail.com>
2024-03-24 14:07:19 +0300

    cryptography

--- backend/reactdjango/reactdjango/settings.py
+++ backend/reactdjango/reactdjango/settings.py
@@ -28,7 +28,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-   'django_daraja',
+
     # adding commas
     'django.contrib.humanize',
      'app',
